1630627740c0645240b267d306481b32664700a5
Admin <Admin@TEB127118>
2022-10-19 11:47:21 +0200

    Add project files.

---
+++ Inlämningar Loops/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Uppgift_4._22
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            for (int i = 10; i <31; i++)
+            {
+                Console.WriteLine(i);
+            }
+            
+            for (int j = 200; j >179; j--)
+            {
+                Console.WriteLine(j);
+            }
+
+            for (int g = 1000; g <1450; g+=50)
+            {
+                Console.WriteLine(g);
+            }
+
+
+
+        }
+    }
+}
